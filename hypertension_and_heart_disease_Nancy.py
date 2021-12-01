8c848b9ed352dcff93e989fa4cd72379894c39c3
Nancyolo <91994754+Nancyolo@users.noreply.github.com>
2021-12-01 11:50:24 -0800

    updated_hypertension_heart_disease

--- hypertension_and_heart_disease_Nancy.py
+++ hypertension_and_heart_disease_Nancy.py
@@ -17,15 +17,15 @@ def bivariate(df):
     b = list(grp.loc[(1,0):(1,1)].id)
     plt.style.use('seaborn')
     bar_width = 0.35
-    dist_1 = np.array([a[0], b[0]])/(a[0]+b[0])
-    dist_2 = np.array([a[1], b[1]]) / (a[1] + b[1])
+    dist_1 = np.array([a[0], b[0]])/(a[0]+b[0]) * 100
+    dist_2 = np.array([a[1], b[1]]) / (a[1] + b[1]) * 100
     a = [dist_1[0],dist_2[0]]
     b = [dist_1[1],dist_2[1]]
     index = np.arange(2)
     rects1 = plt.bar(index, a, bar_width, label='not stroke')
     rects2 = plt.bar(index + bar_width, b, bar_width, label='stroke')
     plt.xticks([bar_width/2,bar_width/2+1], ('not hypertension', 'hypertension'))
-    plt.ylabel('rate', rotation=60)
+    plt.ylabel('percent', rotation=60)
     plt.title('hypertension vs stroke')
     plt.legend()
     # hear disease vs stroke
@@ -33,15 +33,15 @@ def bivariate(df):
     grp = df.groupby(['stroke','heart_disease']).count()
     a = list(grp.loc[(0,0):(0,1)].id)
     b = list(grp.loc[(1,0):(1,1)].id)
-    dist_1 = np.array([a[0], b[0]]) / (a[0] + b[0])
-    dist_2 = np.array([a[1], b[1]]) / (a[1] + b[1])
+    dist_1 = np.array([a[0], b[0]]) / (a[0] + b[0]) * 100
+    dist_2 = np.array([a[1], b[1]]) / (a[1] + b[1]) * 100
     a = [dist_1[0], dist_2[0]]
     b = [dist_1[1], dist_2[1]]
     index = np.arange(2)
     rects1 = plt.bar(index, a, bar_width, label='not stroke')
     rects2 = plt.bar(index + bar_width, b, bar_width, label='stroke')
     plt.xticks([bar_width / 2, bar_width / 2 + 1], ('not heart_disease', 'heart_disease'))
-    plt.ylabel('rate', rotation=60)
+    plt.ylabel('percent', rotation=60)
     plt.title('heart_disease vs stroke')
     plt.legend()
     plt.show()
