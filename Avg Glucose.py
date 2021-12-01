4c23dbfc9bbfa66b228be47ddf9587c01f85a897
wkn20040405 <54109598+wkn20040405@users.noreply.github.com>
2021-12-01 11:16:40 -0800

    Add files via upload

--- Avg Glucose.py
+++ Avg Glucose.py
@@ -11,7 +11,7 @@ data.drop('id', inplace=True, axis=1)
 # data.corr()
 sns.heatmap(data.corr(),cmap='Dark2_r',annot=True)
 plt.title("Correlation")
-plt.show()
+#plt.show()
 #Change other columns to numeric data
 Another_data = data.copy()
 Another_data['gender'] = np.where((Another_data['gender'] == "Female"), 1, 0)
@@ -23,7 +23,7 @@ Another_data['smoking_status'] = Another_data['smoking_status'].factorize()[0]
 plt.figure(figsize=(9,9))
 sns.heatmap(Another_data.corr(),annot=True, linewidths=0.1)
 plt.title("Expanded Correlation")
-plt.show()
+#plt.show()
 
 # Below is the Avg_glucose_level Analysis part
 fig, ax= plt.subplots(figsize=(9,7))
@@ -31,4 +31,16 @@ sns.histplot(x="avg_glucose_level",hue = "stroke", data=data, ax=ax)
 plt.xlabel("Avg_glucose_level")
 plt.ylabel("Count")
 #plt.legend(loc='upper right', title='Residence_type:', labels=['Urban','Rural'])
+#plt.show()
+
+
+#add bmi status column (changing the bmi value to underweight,normal and overweight)
+criteria_glucose = [data['avg_glucose_level'].between(0, 70), data['avg_glucose_level'].between(70.0001, 140), data['avg_glucose_level'].between(140.0001, 3000)]
+values = ['low', 'normal', 'high']
+data['glucose_level_status'] = np.select(criteria_glucose, values, 0)
+
+#normalized plot for bmi and stroke
+x,y = 'glucose_level_status', 'stroke'
+
+data.groupby(x)[y].value_counts(normalize=True).mul(100).rename('percent').reset_index().pipe((sns.catplot,'data'), x=x, y='percent', hue=y, kind='bar')
 plt.show()
